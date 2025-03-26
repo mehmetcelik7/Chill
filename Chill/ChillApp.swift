f14bf4de582d74ed21afcb4964696443b44ab384
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-26 17:50:24 +0300

    Initial Commit

---
+++ Chill/ChillApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChillApp.swift
+//  Chill
+//
+//  Created by mehmet Çelik on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
